eec4e5690b1d79d7868f5a50543db74da644a72e
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2018-08-06 14:32:08 +0530

    Create OddorEven.java

---
+++ OddorEven.java
@@ -0,0 +1,16 @@
+import java.io*;
+import java.util.Scanner;
+class OddorEven
+{
+    public static void main(String[] args)
+    {
+        int x;
+        System.out.println("Enter the number");
+        Scanner n = new Scanner(System.in);
+        x = n.nextInt();
+        if(x%==0)
+            System.out.println("The number is even");
+         else
+             System.out.println("The number is odd");
+       }
+  }
