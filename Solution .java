9e31560ee0efaff8c9695489deeb703d3e9a809b
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2018-09-03 13:37:33 +0530

    Update and rename Solu .java to Solution .java

---
+++ Solution .java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Solution 
+{
+    public static void main(String args[] ) 
+
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        for (int i = 0; i < N; i++) 
+{
+            System.out.println("hello");
+        }
+    }
+}
